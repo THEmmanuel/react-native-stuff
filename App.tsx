46fd59cae4398a60ec33d1dc4a461a161430ed35
Emmanuel Ayodele <emmayodayo@gmail.com>
2021-04-19 11:45:40 +0100

    Some state stuff

--- App.tsx
+++ App.tsx
@@ -1,18 +1,33 @@
 /* eslint-disable react-native/no-inline-styles */
 /* eslint-disable prettier/prettier */
 
-import React from 'react';
-import { Text, View, Image } from 'react-native';
+import React, {useState } from 'react';
+import { View, Text, Button } from 'react-native';
+
+const Cat = (props: any) => {
+    const [isHungry, setIsHungry] = useState(true);
+
+    return (
+        <View>
+            <Text>
+                Hi, I'm {props.name}, and i'm {isHungry ? 'hungry' : 'okay'}
+            </Text>
+
+            <Button
+                onPress={() => setIsHungry(false)}
+                disabled={!isHungry}
+                title={isHungry ? 'pour me some milk hooman!' : 'pfft, whatever'}
+            />
+        </View>
+    );
+};
 
 const App = () => {
     return (
-        <View>
-            <Image
-                source={{ uri: 'https://reactnative.dev/docs/assets/p_cat1.png' }}
-                style={{ width: 200, height: 200 }}
-            />
-            <Text>Well, hello. I'm a cat!</Text>
-        </View>
+        <>
+            <Cat name="Tom" />
+            <Cat name="Ginger" />
+        </>
     );
 };
 
