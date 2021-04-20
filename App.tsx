3991b6fbb5e56afbea2e3e5b74cf3203ff36f32c
Emmanuel Ayodele <emmayodayo@gmail.com>
2021-04-20 11:50:11 +0100

    ListViews {SectionLists!}

--- App.tsx
+++ App.tsx
@@ -1,43 +1,53 @@
 /* eslint-disable prettier/prettier */
 
-
 import React from 'react';
-import { FlatList, StyleSheet, Text, View } from 'react-native';
+import { SectionList, StyleSheet, Text, View } from 'react-native';
 
-const styles = StyleSheet.create(
-    {
-        container: {
-            flex: 1,
-            paddingTop: 22,
-        },
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        paddingTop: 22,
+    },
 
-        item: {
-            padding: 10,
-            fontSize: 18,
-            height: 44,
-        },
-    }
-);
+    sectionHeader: {
+        paddingTop: 2,
+        paddingLeft: 10,
+        paddingRight: 10,
+        paddingBottom: 2,
+        fontSize: 14,
+        fontWeight: 'bold',
+        backgroundColor: '#e2e2e2',
+    },
+
+    item: {
+        padding: 10,
+        fontSize: 18,
+        height: 44,
+    },
+});
+
 
 const App = () => {
     return (
         <View style={styles.container}>
-            <FlatList
-                data={
-                    [
-                        { key: 'Emmanuel' },
-                        { key: 'Ben' },
-                        { key: 'Jude' },
-                        { key: 'Mark' },
-                        { key: 'Luna' },
-                        { key: 'Drew' },
-                        { kwy: 'Seun' },
-                        { key: 'Jimmy' },
-                    ]
-                }
-                // renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
-                renderItem={({ item }) => <Text style={styles.item}>{item.key}</Text>}
+            <SectionList sections={
+                [
+                    { title: 'D', data: ['Dan', 'Devin', 'Dummy'] },
+                    { title: 'E', data: ['Emmanuel', 'Ella', 'Eugene'] },
+                ]
+            }
+                renderItem={({ item }) => <Text style={styles.item}>
+                    {item}
+                </Text>}
+
+                renderSectionHeader={({ section }) => <Text style={styles.sectionHeader}>
+                    {section.title}
+                </Text>}
+
+                keyExtractor={(item, index) => index}
+
             />
+
         </View>
     );
 };
